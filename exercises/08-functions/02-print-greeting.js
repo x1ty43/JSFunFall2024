3613a4d41bf1cf586f2daff5c4cb9d08e32a2890
Matina Patsos <matinaspatsos@gmail.com>
2024-10-09 19:00:59 -0400

    Function exercises (#9)
    * Functions exercises
    
    * Correct instructions

---
+++ exercises/08-functions/02-print-greeting.js
@@ -0,0 +1,14 @@
+/**
+ * Create a function named called "printGreeting".
+ * It should accept a "name" as a parameter
+ * and print "Hello ______!" with console.log
+ *
+ * @param {string} name
+ *
+ * @example printGreeting("Tim"); // Hello Tim!
+ * You should use arrow syntax.
+ *
+ * @warning if you use console.log  more than once, they can get a false negative, even if your answer is correct
+ */
+
+// WRITE YOUR ANSWER BELOW THIS LINE
